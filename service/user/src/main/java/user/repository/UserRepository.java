b5cb83753b9e4aae41441a59393837ab1b495f64
김현우 <162122452+HyunwooKiim@users.noreply.github.com>
2025-12-09 10:25:38 +0900

    feat(UserRepository.java): User 리포지토리 인터페이스 추가

---
+++ service/user/src/main/java/user/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package user.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import user.model.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+    boolean existsByUsername(String username);
+}

704c1c828475d8f0000f8b56a4fc7342ad7f9f09
김현우 <162122452+HyunwooKiim@users.noreply.github.com>
2025-12-10 19:09:23 +0900

    feat(SignupRequest.java): 회원가입 요청 DTO 추가

---
+++ service/user/src/main/java/user/dto/SignupRequest.java
@@ -0,0 +1,18 @@
+package user.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignupRequest {
+    @NotBlank
+    private String username;
+    @NotBlank
+    private String password;
+    @NotBlank
+    private String nickname;
+}

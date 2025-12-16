575abe8c75b691375f6aee12e7eb862557a3c3b0
김현우 <162122452+HyunwooKiim@users.noreply.github.com>
2025-12-16 10:42:57 +0900

    feat(CommentRequest.java): Comment 요청 DTO 추가

---
+++ service/comment/src/main/java/comment/dto/CommentRequest.java
@@ -0,0 +1,18 @@
+package comment.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentRequest {
+    @NotNull
+    private Long articleId;
+    @NotBlank
+    private String content;
+    private Long parentId;
+}
